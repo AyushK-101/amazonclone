401093c418341271793a9f821edfb05c4cf55245
Ayush Kumar <ayushbhaiya100@gmail.com>
2023-02-19 13:12:20 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCJzTSQ09DCA9qYBxUESQVHJYVKzWf2Bpc",
+  authDomain: "clone-d8302.firebaseapp.com",
+  projectId: "clone-d8302",
+  storageBucket: "clone-d8302.appspot.com",
+  messagingSenderId: "498137631903",
+  appId: "1:498137631903:web:c6b1ec2a1fdc2ed18f66aa"
+};
